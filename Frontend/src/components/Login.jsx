27db4e09183f6ea7438f3f3659e98e56c81e7b46
TezAryan <tezaryan2004@gmail.com>
2025-03-27 00:21:18 +0530

    first commit

---
+++ Frontend/src/components/Login.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import Image from "../assets/image.jpg";
+import Logo from "../assets/logo.png";
+import GoogleSvg from "../assets/icons8-google.svg";
+import { FaEye } from "react-icons/fa6";
+import { FaEyeSlash } from "react-icons/fa6";
+
+
+
+const Login = () => {
+  const [ showPassword, setShowPassword ] = useState(false);
+
+
+  return (
+    <div className="login-main">
+      <div className="login-left">
+        <img  className="image" src={Image} alt="" />
+      </div>
+      <div className="login-right">
+        <div className="login-right-container">
+          <div className="login-logo">
+            <img src={Logo} alt="" />
+             
+          </div>
+          <div className="login-center">
+            <h2>Welcome back!</h2>
+            <p>Please enter your details</p>
+            <form>
+              <input type="email" placeholder="Username" />
+              <div className="pass-input-div">
+                <input type={showPassword ? "text" : "password"} placeholder="Password" />
+                {showPassword ? <FaEyeSlash onClick={() => {setShowPassword(!showPassword)}} /> : <FaEye onClick={() => {setShowPassword(!showPassword)}} />}
+                
+              </div>
+
+             
+             
+            </form>
+          </div>
+
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
